8f96970c8d59d71748825bf287660bcdbe035a32
aligate <aligate26@gmail.com>
2017-02-28 20:43:47 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,7 +5,6 @@ $name = __DIR__.'/'.$_FILES['test']['name'];
 if(move_uploaded_file($temp_name, $name)) header('Location: list.php');
 }
 ?>
-
 <!doctype html>
 <html>
   <head>
@@ -14,13 +13,12 @@ if(move_uploaded_file($temp_name, $name)) header('Location: list.php');
     
   </head>
   <body>
-
-    <h2>Загрузка тестовых файлов</h2>
+<h2>Загрузка тестовых файлов</h2>
 
    <form method = 'POST' enctype='multipart/form-data'>
 		<input type ='file' name='test'>
 		<button>Отправить</button>
 	</form>
-	
+	  <p style ="color: blue; font-size:20px;"><a href="list.php">Перейти к списку загруженных файлов</a></p>
   </body>
 </html>
